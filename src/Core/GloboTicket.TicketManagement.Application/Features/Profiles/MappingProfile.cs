eebc94cf86de7d600ccc5634cb7c951caee2bc5c
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-08-16 12:51:14 +0200

    Add queries for getting events list and an event details.

---
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Features.Profiles;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Event, EventListVm>().ReverseMap();
+        CreateMap<Event, EventDetailVm>().ReverseMap();
+        CreateMap<Category, CategoryDto>().ReverseMap();
+    }
+    
+}
