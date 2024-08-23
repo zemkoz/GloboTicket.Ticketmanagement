cafad02fd52f6b959d6d5f6210da3632c429daac
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-08-23 11:54:06 +0200

    Add sending e-mail notification to the admin when an event is created.

--- src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Exceptions;
+using GloboTicket.TicketManagement.Application.Models;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 
@@ -9,11 +12,15 @@ public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Gui
 {
     private readonly IEventRepository _eventRepository;
     private readonly IMapper _mapper;
+    private readonly IEmailService _emailService;
 
-    public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+    public CreateEventCommandHandler(IMapper mapper, 
+        IEventRepository eventRepository, 
+        IEmailService emailService)
     {
         _mapper = mapper;
         _eventRepository = eventRepository;
+        _emailService = emailService;
     }
 
     public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -22,11 +29,33 @@ public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Gui
         var validationResult = await validator.ValidateAsync(request);
         if (validationResult.Errors.Count > 0)
         {
-            throw new Exceptions.ValidationException(validationResult);
+            throw new ValidationException(validationResult);
         }
         
         var @event = _mapper.Map<Event>(request);
         @event = await _eventRepository.AddAsync(@event);
+
+        await SendNotificationEmailToAdmin(request);
+
         return @event.EventId;
+    }
+
+    private async Task SendNotificationEmailToAdmin(CreateEventCommand request)
+    {
+        var email = new Email()
+        {
+            To = "admin@test.com",
+            Subject = "A new event was created",
+            Body = $"A new event was created: {request}"
+        };
+
+        try
+        {
+            await _emailService.SendEmail(email);
+        }
+        catch (Exception e)
+        {
+            // This shouldn't stop the API from doing else so this can be logged.
+        }
     }
 }
