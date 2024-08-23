7c79f74b8d4e0fd07552aead04d20f5c3921232d
Zoltan Zemko <zoltan.zemko@hartmann.info>
2024-08-23 12:14:25 +0200

    Implement an email service

---
+++ src/Infrastructure/GloboTickets.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTickets.TicketManagement.Infrastructure.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTickets.TicketManagement.Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<EmailService>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
+        
+        return services;
+    }
+}
---
+++ src/Infrastructure/GloboTickets.TicketManagement.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace GloboTickets.TicketManagement.Infrastructure.Mail;
+
+public class EmailService : IEmailService
+{
+    private readonly EmailSettings _emailSettings;
+    
+    public EmailService(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+    
+    public async Task<bool> SendEmail(Email email)
+    {
+        var client = new SendGridClient(_emailSettings.ApiKey);
+
+        var fromAddress = new EmailAddress()
+        {
+            Email = _emailSettings.FromAddress,
+            Name = _emailSettings.FromName
+        };
+        var toAddress = new EmailAddress(email.To);
+        
+        var sendGridMessage = MailHelper.CreateSingleEmail(
+            fromAddress, 
+            toAddress, 
+            email.Subject,
+            email.Body, 
+            email.Body);
+
+        var response = await client.SendEmailAsync(sendGridMessage);
+        return response.StatusCode is HttpStatusCode.Accepted or HttpStatusCode.OK;
+    }
+}
